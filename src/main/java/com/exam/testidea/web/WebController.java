4df593f3ddd989b66788dd7a8bbb6408b7aa5141
边边 <xinqian_han@outlook.com>
2019-06-13 16:31:18 +0800

    git idea

--- src/main/java/com/exam/testidea/web/WebController.java
+++ src/main/java/com/exam/testidea/web/WebController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("web")
 public class WebController {
-    //
+    //这是什么鬼
     @RequestMapping("send")
     public String sendString(){
+        //返回一个什么鬼的东西噢
         return "git与idea测试。";
     }
 }
